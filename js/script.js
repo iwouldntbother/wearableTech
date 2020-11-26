e662b7953ba31c010892f6b25038d70557c6db8b
wwstwd <65234242+wwstwd@users.noreply.github.com>
2020-11-26 03:09:28 +0000

    largeUpdate

--- js/script.js
+++ js/script.js
@@ -4,9 +4,4 @@ document.addEventListener("scroll", function(){
     var imageMarginTop = Math.min(100, Math.max(10, (100 - (scrollPercent * 90))))
 
     image.style.marginTop = String(imageMarginTop) + "vh"
-
-    console.log(scrollPercent)
-    // console.log((100 - (scrollPercent * 90)))
-    // console.log(Math.min(100, Math.max(10, (100 - (scrollPercent * 90)))))
-
 })
